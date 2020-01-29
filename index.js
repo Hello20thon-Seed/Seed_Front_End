b4a45cf45388a92efdb73221df628397b0708f46
HomeDesktop <ajy720@gmail.com>
2020-01-30 00:47:35 +0900

    Creat addTable page

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ var plus = document.getElementById('plus-goal')
 plus.addEventListener("click", function() {
   plus.innerHTML = "목표추가";
 });
+
+var prev = document.getElementById('prev')
+
+var wrap = document.getElementByClassName('wrap')
+var how = document.getElementByClassName('how')
+var next = document.getElementById('next')
+next.addEventListener('click', function(){
+	wrap.innerHTML('how')	
+})
