7bffd6ab509055776f07eba52f9ba65cd480d4dd
HomeDesktop <ajy720@gmail.com>
2020-01-30 18:01:03 +0900

    MainPage Design

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var plus = document.getElementById('plus-goal')
 plus.addEventListener("click", function() {
-  plus.innerHTML = "목표추가";
+ 	plus.innerHTML = "목표추가";
 });
 
 var prev = document.getElementById('prev')
@@ -8,11 +8,3 @@ var prev = document.getElementById('prev')
 var wrap = document.getElementByClassName('wrap')
 var how = document.getElementByClassName('how')
 var next = document.getElementById('next')
-next.addEventListener('click', function(){
-<<<<<<< HEAD
-	wrap.innerHTML('how')	
-=======
-	wrap.innerHTML('how')
-	
->>>>>>> 8e95adffbc27d51a8dc74ed16b66b4e54b04ff61
-})
