059f40ef72cfe932490b05eb92188763a211cf1b
HomeDesktop <ajy720@gmail.com>
2020-02-01 22:05:44 +0900

    SideMenu Tree

--- js/renderTree.js
+++ js/renderTree.js
@@ -1,41 +1,42 @@
-$(function() {
-    $.mockjax({
-        url: '/orgchart/initdata',
-        responseTime: 1000,
-        contentType: 'application/json',
-        responseText: {
-            'name': 'Lao Lao',
-            'title': 'general manager',
-            'children': [
-                { 'name': 'Bo Miao', 'title': 'department manager' },
-                { 'name': 'Su Miao', 'title': 'department manager',
-                'children': [
-                    { 'name': 'Tie Hua', 'title': 'senior engineer',
-                    'children': [
-                    { 'name': 'Pang Pang', 'title': 'engineer' }
-                    ] },
-                    { 'name': 'Hei Hei', 'title': 'senior engineer',
-                    'children': [
-                        { 'name': 'Pang Pang', 'title': 'engineer' },
-                        { 'name': 'Xiang Xiang', 'title': 'UE engineer' }
-                    ]
-                    }
-                ]
-                },
-                { 'name': 'Yu Jie', 'title': 'department manager' }
-            ]
-        }
+function renderTree() {
+    var datasource = {
+      'name': 'Lao Lao',
+      'title': 'general manager',
+      'children': [
+        { 'name': 'Bo Miao', 'title': 'department manager' },
+        { 'name': 'Su Miao', 'title': 'department manager',
+          'children': [
+            { 'name': 'Tie Hua', 'title': 'senior engineer' },
+            { 'name': 'Hei Hei', 'title': 'senior engineer',
+              'children': [
+                { 'name': 'Dan Dan', 'title': 'engineer' }
+              ]
+            },
+            { 'name': 'Pang Pang', 'title': 'senior engineer' }
+          ]
+        },
+        { 'name': 'Hong Miao', 'title': 'department manager' }
+      ]
+    };
+
+    var oc = $('#chart-container').orgchart({
+      'data' : datasource,
+      'nodeContent': 'title',
+      'pan':true,
+      'zoom':true
     });
 
-    $('#chart-container').orgchart({
-        'data' : '/orgchart/initdata',
+    var oc2 = $('#chart-container2').orgchart({
+        'data' : datasource,
         'nodeContent': 'title',
-        'pan': true,
-        'zoom': true
-    });
+      });
 
     oc.$chartContainer.on('touchmove', function(event) {
         event.preventDefault();
     });
 
-});
+    oc2.init({'verticalLevel': 2});
+
+};
+
+renderTree();
